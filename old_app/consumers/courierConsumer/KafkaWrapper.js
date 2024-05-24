f0662d91283a012ce1b4dae11bd9cb6f16fd2fa7
AlexLiviaStan <161331149+AlexLiviaStan@users.noreply.github.com>
2024-05-24 22:51:41 +0100

    consumer - Kafka connection

--- old_app/consumers/courierConsumer/KafkaWrapper.js
+++ old_app/consumers/courierConsumer/KafkaWrapper.js
@@ -20,10 +20,10 @@ class KafkaWrapper {
         let driver_options = {
             //'debug': 'all',
             'metadata.broker.list': brokers,
-            //'security.protocol': protocol,
-            //'sasl.mechanisms': mechanism,
-            //'sasl.username': username,
-            //'sasl.password': password,
+            'security.protocol': protocol,
+            'sasl.mechanisms': mechanism,
+            'sasl.username': username,
+            'sasl.password': password,
             'log.connection.close' : false,
             'enable.auto.commit': false
         };
@@ -67,10 +67,10 @@ class KafkaWrapper {
         let producer = new Kafka.Producer(producerConfig, producerTopicConfig)
         producer.setPollInterval(100)
 
-        // debug
-        // producer.on('event.log', function(log) {
-        //     console.log(log);
-        // });
+        //debug
+        producer.on('event.log', function(log) {
+             console.log(log);
+         });
 
         // Register error listener
         producer.on('event.error', function(err) {
@@ -91,7 +91,7 @@ class KafkaWrapper {
 
         this.producer = producer
     }
-  
+
     on(event, callback) {
       this.consumer.on(event, callback)
     }
@@ -131,11 +131,11 @@ class KafkaWrapper {
 }
 
 // const kafkaWrapper = new KafkaWrapper(process.env.KAFKA_CREDENTIALS)
-const kafkaWrapper = new KafkaWrapper(process.env.BOOTSTRAP_SERVERS,
-                                      process.env.SECURITY_PROTOCOL,
-                                      process.env.SASL_MECHANISMS,
-                                      process.env.SASL_USERNAME,
-                                      process.env.SASL_PASSWORD)
+const kafkaWrapper = new KafkaWrapper(process.env.KAFKA_BOOTSTRAP_SERVERS,
+                                      process.env.KAFKA_SECURITY_PROTOCOL,
+                                      process.env.KAFKA_SASL_MECHANISMS,
+                                      process.env.KAFKA_SASL_USERNAME,
+                                      process.env.KAFKA_SASL_PASSWORD)
 Object.freeze(kafkaWrapper)
 
 module.exports = kafkaWrapper
