a7f6eafa9cabe1073911c78290df45415a3bdff3
AlexLiviaStan <161331149+AlexLiviaStan@users.noreply.github.com>
2024-06-17 11:35:29 +0100

    Update KafkaWrapper.js
    Adding the Kafka config

--- old_app/consumers/orderConsumer/KafkaWrapper.js
+++ old_app/consumers/orderConsumer/KafkaWrapper.js
@@ -1,228 +1,164 @@
-const KafkaWrapper = require('./KafkaWrapper.js')
-const mongoose = require('mongoose');
-const MUUID = require('uuid-mongodb').mode('relaxed');
-const Order = require('./models/order.js')
+const Kafka = require('node-rdkafka');
 
-// environment variables for mongodb connection
-const MONGODB_REPLICA_HOSTNAMES = process.env.MONGODB_REPLICA_HOSTNAMES
-const MONGODB_REPLICA_SET = process.env.MONGODB_REPLICA_SET
-const MONGODB_DBNAME = process.env.MONGODB_DBNAME
-const MONGODB_AUTH_DBNAME = process.env.MONGODB_AUTH_DBNAME || MONGODB_DBNAME
-const MONGODB_CA_PATH = process.env.MONGODB_CA_PATH
-const MONGODB_USER = process.env.MONGODB_USER
-const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD
-mongoose
-    .connect('mongodb://' + MONGODB_REPLICA_HOSTNAMES + '/', {
-        user: MONGODB_USER,
-        pass: MONGODB_PASSWORD,
-        dbName: MONGODB_DBNAME,
-        replicaSet: MONGODB_REPLICA_SET,
-        authSource: MONGODB_AUTH_DBNAME,
-        tls: true,
-        tlsCAFile: MONGODB_CA_PATH,
-        useCreateIndex: true,
-        useNewUrlParser: true,
-        useUnifiedTopology: true})
-    .catch(error => {
-        console.log(error)
-        process.exit(1)
-    });
+class KafkaWrapper {
+    constructor(brokers, protocol, mechanism, username, password) {
+        // ibm cloud service credentials
+        // let jsonCredentials = JSON.parse(ibmcloud_credentials)
+        // let brokers = jsonCredentials.kafka_brokers_sasl
+        // let apiKey = jsonCredentials.api_key
+        // producer
+        // let driver_options = {
+        //     //'debug': 'all',
+        //     'metadata.broker.list': brokers,
+        //     'security.protocol': 'SASL_SSL',
+        //     'sasl.mechanisms': 'PLAIN',
+        //     'sasl.username': 'token',
+        //     'sasl.password': apiKey,
+        //     'log.connection.close' : false,
+        //     'enable.auto.commit': false
+        // };
+        /*let driver_options = {
+            //'debug': 'all',
+            'metadata.broker.list': "broker-3-b1392rt9v4gd0bpb.kafka.svc10.us-south.eventstreams.cloud.ibm.com:9093,broker-4-b1392rt9v4gd0bpb.kafka.svc10.us-south.eventstreams.cloud.ibm.com:9093,broker-2-b1392rt9v4gd0bpb.kafka.svc10.us-south.eventstreams.cloud.ibm.com:9093",
+            'security.protocol': "SASL_SSL",
+            'sasl.mechanisms': "PLAIN",
+            'sasl.username': "token",
+            'sasl.password': "7qs10VpEP8MvpWny4ZQQv55_VEo6f8ZPjFD7QHZrPqwO",
+            'log.connection.close' : false,
+            'ssl.ca.location': '/etc/ssl/certs'
+        };
+        */
 
-KafkaWrapper.consumer.on('ready', function() {
-    console.log('The consumer has connected.');
-    KafkaWrapper.consumer.subscribe(['orders']);
-    KafkaWrapper.consumer.consume()
-}).on('data', function(data) {
-    try {
-        let dataObject = JSON.parse(data.value.toString())
-        // dataObject
-        // {eventType, payload: {orderId,userId,kitchenId,requestId}, simulatorConfig}
-        let eventType = dataObject.eventType
-        let order = dataObject.payload
-        let simulatorConfig = dataObject.simulatorConfig
-        switch (eventType) {
-            case "orderRequested":
-                createOrderDocument(order, err => {
-                    let statusMessage
-                    if (err) {
-                        console.log("error saving document")
-                        console.error(err)
-                        statusMessage = {status: "Order failed saving in database"}
-                    } else {
-                        console.log(`Order ID ${order.orderId} saved`)
-                        statusMessage = {status: "orderCreated"}
-                    }
-                    KafkaWrapper.updateHttpResponse({requestId: order.requestId, message: JSON.stringify(statusMessage)}, simulatorConfig, (err) => {
-                        if (err) console.error(err)
-                    })
-                    // produce kafka message with eventType orderCreated
-                    KafkaWrapper.createdOrderEvent(order, simulatorConfig, err => {
-                        if (err) {
-                            console.log("error producing event")
-                            console.error(err)
-                        } else {
-                            console.log(`orderCreated event for ${order.orderId} created`)
-                        }
-                    })
-                    KafkaWrapper.consumer.commitMessage(data)
-                })
-                break;
-            case "orderCreated":
-                setTimeout(() => {
-                    setOrderStatusTo(order.orderId, 'orderValidated', (err, doc) => {
-                        if (err) {
-                            console.error(err)
-                        } else {
-                            console.log(`Order ${order.orderId} is validated.`)
-                            // kitchen consumer will handle orderValidated
-                            KafkaWrapper.validatedEvent(order, simulatorConfig, err => {
-                                if (err) {
-                                    console.log("error producing event")
-                                    console.error(err)
-                                } else {
-                                    console.log(`orderValidated event for ${order.orderId} created`)
-                                }
-                            })
-                        }
-                    })
-                }, 5000)
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "kitchenPreparingFood":
-                setOrderStatusTo(order.orderId, 'kitchenPreparingFood', (err, doc) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log(`Order ${order.orderId} status updated to kitchenPreparingFood`)
-                    }
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "kitchenFoodReady":
-                setOrderStatusTo(order.orderId, 'kitchenFoodReady', (err, doc) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log(`Order ${order.orderId} status updated to kitchenFoodReady`)
-                    }
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "courierMatched":
-                setOrderStatusTo(order.orderId, 'courierMatched', (err, doc) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log(`Order ${order.orderId} status updated to courierMatched`)
-                    }
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "courierPickedUp":
-                setOrderStatusTo(order.orderId, 'courierPickedUp', (err, doc) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log(`Order ${order.orderId} status updated to courierPickedUp`)
-                    }
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "delivered":
-                setOrderStatusTo(order.orderId, 'delivered', (err, doc) => {
-                    if (err) {
-                        console.error(err)
-                    } else {
-                        console.log(`Order ${order.orderId} status updated to delivered`)
-                    }
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "getOrderFromId":
-                getOrderFromId(order.orderId, (err, doc) => {
-                    let message
-                    if (err) {
-                        console.log("error getting restaurants")
-                        console.error(err);
-                        message = {status: `error getting order of ${order.orderId}`}
-                    } else {
-                        message = {status: "success", doc}
-                    }
-                    KafkaWrapper.updateHttpResponse({requestId: order.requestId, message: JSON.stringify(message)}, simulatorConfig, (err) => {
-                        if (err) console.error(err)
-                    })
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            case "getOrdersOfUser":
-                getOrdersOfUser(order.userId, (err, docs) => {
-                    let message
-                    if (err) {
-                        console.log("error getting restaurants")
-                        console.error(err);
-                        message = {status: `error getting orders of user ${order.userId}`}
-                    } else {
-                        message = {status: "success", docs}
-                    }
-                    KafkaWrapper.updateHttpResponse({requestId: order.requestId, message: JSON.stringify(message)}, simulatorConfig, (err) => {
-                        if (err) console.error(err)
-                    })
-                })
-                KafkaWrapper.consumer.commitMessage(data)
-                break;
-            default:
-                console.log(`${dataObject.eventType} is not handled in this service`)
-                KafkaWrapper.consumer.commitMessage(data)
+
+/*         console.log('KAFKA_BOOTSTRAP_SERVERS:'+ process.env.KAFKA_BOOTSTRAP_SERVERS+'/');
+
+         console.log('KAFKA_SECURITY_PROTOCOL:'+ process.env.KAFKA_SECURITY_PROTOCOL+'/');
+
+         console.log('KAFKA_SECURITY_MECHANISM:'+ process.env.KAFKA_SASL_MECHANISMS+'/');
+
+         console.log('KAFKA_SASL_USERNAME:'+ process.env.KAFKA_SASL_USERNAME+'/');
+
+         console.log('KAFKA_SASL_PASSWORD:'+ process.env.KAFKA_SASL_PASSWORD+'/');
+        */
+
+        let driver_options = {
+            //'debug': 'all',
+            'metadata.broker.list': brokers,
+            'security.protocol': protocol,
+            'sasl.mechanisms': mechanism,
+            'sasl.username': username,
+            'sasl.password': password,
+            'log.connection.close': false,
+            'ssl.ca.location': '/etc/ssl/certs'
+        };
+
+
+        let consumerConfig = {
+            'client.id': 'orders-consumer',
+            'group.id': 'orders-consumer-group',
         }
-    } catch (err) {
-        console.error(err)
-        // add error response to redis
-        KafkaWrapper.consumer.commitMessage(data)
+
+        for (var key in driver_options) {
+            consumerConfig[key] = driver_options[key]
+        }
+
+        // create kafka consumer
+        let topicConfig = {
+            'auto.offset.reset': 'earliest'
+        }
+        let consumer = new Kafka.KafkaConsumer(consumerConfig, topicConfig)
+
+        // Register error listener
+        consumer.on('event.error', function(err) {
+            console.error('Error from consumer:' + JSON.stringify(err));
+        });
+
+        this.consumer = consumer
+
+        // producer
+        let producerConfig = {
+            'client.id': 'example-orders-producer',
+            'dr_msg_cb': true  // Enable delivery reports with message payload
+        }
+
+        for (var key in driver_options) {
+            producerConfig[key] = driver_options[key]
+        }
+
+        // create kafka producer
+        let producerTopicConfig = {
+            'request.required.acks': -1
+            // 'produce.offset.report': true
+        }
+        let producer = new Kafka.Producer(producerConfig, producerTopicConfig)
+        producer.setPollInterval(100)
+
+        // debug
+        // producer.on('event.log', function(log) {
+        //     console.log(log);
+        // });
+
+        // Register error listener
+        producer.on('event.error', function(err) {
+            console.error('Error from producer:' + JSON.stringify(err));
+        });
+
+        // Register delivery report listener
+        producer.on('delivery-report', function(err, dr) {
+            if (err) {
+                console.error('Delivery report: Failed sending message ' + dr.value);
+                console.error(err);
+                // We could retry sending the message
+            } else {
+                console.log('Message produced, partition: ' + dr.partition + ' offset: ' + dr.offset);
+            }
+        });
+        producer.connect()
+
+        this.producer = producer
     }
-});
 
-function createOrderDocument(payload, callback) {
-    let order = {
-        orderId: MUUID.from(payload.orderId),
-        userId: MUUID.from(payload.userId),
-        kitchenId: MUUID.from(payload.kitchenId),
-        status: "orderCreated",
-        dish: payload.dish,
-        statusHistory: [
-            { status: "orderCreated"}
-        ],
-        totalPrice: payload.totalPrice
+    on(event, callback) {
+      this.consumer.on(event, callback)
     }
-    let newOrder = new Order(order)
-    newOrder.save(err => callback(err))
+
+    createEvent(order, event, simulatorConfig, callback) {
+        let topicName = 'orders'
+        let eventType = event
+        let message = Buffer.from(JSON.stringify({eventType, payload: order, simulatorConfig}))
+        try {
+            this.producer.produce(
+                topicName,
+                null,
+                message,
+                order.orderId
+            )
+            callback(null)
+        } catch (err) {
+            console.log('caught')
+            callback(err)
+        }
+    }
+
+    createdOrderEvent(order, simulatorConfig, callback) {
+        this.createEvent(order, 'orderCreated', simulatorConfig, callback)
+    }
+
+    validatedEvent(order, simulatorConfig, callback) {
+        this.createEvent(order, 'orderValidated', simulatorConfig, callback)
+    }
+
+    updateHttpResponse(payload, simulatorConfig, callback) {
+        this.createEvent(payload, 'updateHttpResponse', simulatorConfig, callback)
+    }
 }
 
-function setOrderStatusTo(orderId, status, callback) {
-    let filter = { orderId: MUUID.from(orderId) }
-    let update = { status, "$push": { statusHistory: {status} } }
+// const kafkaWrapper = new KafkaWrapper(process.env.KAFKA_CREDENTIALS)
+const kafkaWrapper = new KafkaWrapper(process.env.KAFKA_BOOTSTRAP_SERVERS,
+    process.env.KAFKA_SECURITY_PROTOCOL,
+    process.env.KAFKA_SASL_MECHANISMS,
+    process.env.KAFKA_SASL_USERNAME,
+    process.env.KAFKA_SASL_PASSWORD)
+Object.freeze(kafkaWrapper)
 
-    Order.findOneAndUpdate(filter, update, {new: true}, (err, doc) => {
-        callback(err, doc)
-    })
-}
-
-function getOrderFromId(orderId, callback) {
-    let filter = { orderId: MUUID.from(orderId) }
-
-    Order.findOne(filter, {__v: false, _id: false}, (err, doc) => {
-        callback(err, doc)
-    })
-}
-
-function getOrdersOfUser(userId, callback) {
-    let filter = { userId: MUUID.from(userId) }
-
-    Order.find(filter, {__v: false, _id: false}, (err, docs) => {
-        callback(err, docs)
-    })
-}
-
-KafkaWrapper.producer.on('ready', () => {
-    console.log('The producer has connected.')
-    KafkaWrapper.consumer.connect()
-})
+module.exports = kafkaWrapper
